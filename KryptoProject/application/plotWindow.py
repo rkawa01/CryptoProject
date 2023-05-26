04c5edb4eeffaddbcc6652c277f6811e77cdc28e
Thyen-Radek <rabuhrabuhrabuh@poczta.fm>
2023-05-26 03:02:47 +0200

    some changes to the plot

--- KryptoProject/application/plotWindow.py
+++ KryptoProject/application/plotWindow.py
@@ -4,11 +4,15 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QVB
     QPushButton
 import pandas as pd
 import pyqtgraph as pg
+import seaborn as sns
+from matplotlib import pyplot as plt
 import matplotlib
 matplotlib.use('Qt5Agg')
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas, NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 from getCrypto import Crypto
+import mplcursors
+
 
 
 class DateTimeAxis(pg.AxisItem):
@@ -17,7 +21,7 @@ class DateTimeAxis(pg.AxisItem):
         return [pd.to_datetime(value, unit='s').strftime('%Y-%m-%d %H:%M:%S') for value in values]
 
 
-class MplCanvas(FigureCanvasQTAgg):
+class MplCanvas(FigureCanvas):
 
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
@@ -47,61 +51,80 @@ class PlotWindow(QMainWindow):
         # Create a vertical layout to hold the plot placeholder and other widgets
         self.layout = QVBoxLayout(self.central_widget)
 
-        self.userLayout = QHBoxLayout()
+        self.init()
+
+        self.get_plot()
+
+
+
+    def init(self):
+
+        userLayout = QHBoxLayout()
         current_price = self.cryptoInfo.get_price_now()
-        self.textLayout = QVBoxLayout()
-        self.label1 = QLabel("UserName:   " + self.username)
-        self.bitcoinsOwned = QLabel("My bitcoins:   " + str(self.bit) + " BTC")
-        self.walletState = QLabel("My wallet state:   " + str(self.wallet) + " USD")
-        self.currentValue = QLabel("Bitcoin buy price:   " + str(current_price) + " USD")
-        self.bitcoinsValue = QLabel("My Bitcoins value as of now:   " + str(current_price*self.bit) + " USD")
+        textLayout = QVBoxLayout()
+        userName = QLabel("UserName:   " + self.username)
+        bitcoinsOwned = QLabel("My bitcoins:   " + str(self.bit) + " BTC")
+        walletState = QLabel("My wallet state:   " + str(self.wallet) + " USD")
+        currentValue = QLabel("Bitcoin buy price:   " + str(current_price) + " USD")
+        bitcoinsValue = QLabel("My Bitcoins value as of now:   " + str(current_price * self.bit) + " USD")
 
-        self.textLayout.addWidget(self.label1)
-        self.textLayout.addWidget(self.bitcoinsOwned)
-        self.textLayout.addWidget(self.walletState)
-        self.textLayout.addWidget(self.currentValue)
-        self.textLayout.addWidget(self.bitcoinsValue)
+        textLayout.addWidget(userName)
+        textLayout.addWidget(bitcoinsOwned)
+        textLayout.addWidget(walletState)
+        textLayout.addWidget(currentValue)
+        textLayout.addWidget(bitcoinsValue)
 
+        # Create the seaborn FigureCanvas object, which defines a single set of axes as self.axes.
+        # self.sc = MplCanvas(self, width=5, height=4, dpi=100)
+        # toolbar = NavigationToolbar(self.sc, self)
+        plot_layout = QVBoxLayout()
+        # plot_layout.addWidget(toolbar)
+        # plot_layout.addWidget(self.sc)
+        # Both alternatives version of changing the background color of the plot area
+        sns.set(rc={'axes.facecolor': 'black', 'figure.facecolor': 'black', 'axes.grid': False, 'axes.labelcolor': 'white', 'text.color': 'white', \
+                    'xtick.color': 'white', 'ytick.color': 'white', 'axes.edgecolor': 'white', 'axes.titlecolor': 'white'})
+        # This with less options
+        # plt.style.use("dark_background")
+        self.figure = plt.figure()
+        # self.figure.set_facecolor("black")
+        self.canvas = FigureCanvas(self.figure)
+        self.ax = self.figure.add_subplot(111)
+        plot_layout.addWidget(self.canvas)
 
-        # Create the maptlotlib FigureCanvas object, which defines a single set of axes as self.axes.
-        self.sc = MplCanvas(self, width=5, height=4, dpi=100)
-        toolbar = NavigationToolbar(self.sc, self)
-        self.plot_layout = QVBoxLayout()
-        self.plot_layout.addWidget(toolbar)
-        self.plot_layout.addWidget(self.sc)
+        self.figure.canvas.mpl_connect("motion_notify_event", self.hover)
 
-        self.buttonLayout = QHBoxLayout()
         # Create the buttons and connect them to functions
+        buttonLayout = QHBoxLayout()
+
         button1 = QPushButton("Today")
         button1.clicked.connect(lambda: self.update_request("Daily"))
-        self.buttonLayout.addWidget(button1)
+        buttonLayout.addWidget(button1)
 
         button2 = QPushButton("Last Week")
         button2.clicked.connect(lambda: self.update_request("Weekly"))
-        self.buttonLayout.addWidget(button2)
+        buttonLayout.addWidget(button2)
 
         button3 = QPushButton("Last Month")
         button3.clicked.connect(lambda: self.update_request("Monthly"))
-        self.buttonLayout.addWidget(button3)
+        buttonLayout.addWidget(button3)
 
         button4 = QPushButton("This Year")
         button4.clicked.connect(lambda: self.update_request("this Year"))
-        self.buttonLayout.addWidget(button4)
+        buttonLayout.addWidget(button4)
 
         button5 = QPushButton("One year")
         button5.clicked.connect(lambda: self.update_request("Yearly"))
-        self.buttonLayout.addWidget(button5)
+        buttonLayout.addWidget(button5)
 
         # Add the plot layout to the main layout
         # self.layout.addLayout(self.plot_layout)
-        self.userLayout.addLayout(self.textLayout)
-        self.layout.addLayout(self.userLayout)
-        self.layout.addLayout(self.plot_layout)
+        userLayout.addLayout(textLayout)
+        self.layout.addLayout(userLayout)
+        self.layout.addLayout(plot_layout)
         # self.layout.addWidget(self.plot_widget)
-        self.layout.addLayout(self.buttonLayout)
+        self.layout.addLayout(buttonLayout)
         self.layout.addStretch()
 
-        self.get_plot()
     def check_textbox(self):
 
         return True
@@ -115,8 +138,8 @@ class PlotWindow(QMainWindow):
         super(PlotWindow, self).resizeEvent(event)
 
     def get_plot(self):
-        self.sc.axes.clear()
-
+        # self.sc.axes.clear()
+        self.ax.clear()
         # print(self.cryptoInfo.get_price_now())
         exchange_rates = self.cryptoInfo.get_weekly()
         if (self.timeStampType):
@@ -137,16 +160,49 @@ class PlotWindow(QMainWindow):
         highPrices = exchange_rates.get(keySet[0])
         lowPrices = exchange_rates.get(keySet[1])
 
-        x = [time.timestamp() for time in exchange_rates.index]
+        # x = [time.timestamp() for time in exchange_rates.index]
         # x = lowPrices.index.astype('int64')//10**9
+        x = lowPrices.index
         y = lowPrices.values
-
         # plot the pandas DataFrame, passing in the matplotlib Canvas axes.
-        exchange_rates.plot(ax=self.sc.axes, y='low')
+        # TODO
+        # sns.set_style("darkgrid")
+
+        self.sc = sns.lineplot(ax=self.ax, x=x, y=y,color = "orange")
+
+        self.ax.tick_params(axis='x', rotation=15)
+        # print(x[10])
+        # print(lowPrices.index[10])
+
+        # self.min_x, self.max_x = x.min(), x.max()
+        # self.min_y,self.max_y = y.min(), y.max()
+        self.min_x, self.max_x = pd.to_datetime(x.min().timestamp() * 0.95, unit='s'), pd.to_datetime(
+            x.max().timestamp() * 1.05, unit='s')
+        self.min_y, self.max_y = y.min() * 0.95, y.max() * 1.05
+        self.lnx = plt.plot([x.min(),x.min()], [y.min(), y.min()], color='white', linewidth=0.3)
+        self.lny = plt.plot([x.min(),x.max()], [y.min(), y.min()], color='white', linewidth=0.3)
+        self.lnx[0].set_linestyle('None')
+        self.lny[0].set_linestyle('None')
+
+
+
+        # mplcursors.cursor(self.sc, hover=True)
+        # exchange_rates.plot(ax=self.sc.axes, y='low')
 
         # alternatively plot x and y prepared sets of data
         # self.sc.axes.plot(x, y)
-        self.sc.draw()
+        self.canvas.draw()
+        # self.sc.draw()
+        # Update positions after drawing
+
+    def hover(self, event):
+        self.lnx[0].set_data([event.xdata, event.xdata], [self.min_y, self.max_y])
+        # print(self.lnx[0].get_data())
+        self.lnx[0].set_linestyle('--')
+        self.lny[0].set_data([self.min_x,self.max_x], [event.ydata, event.ydata])
+        self.lny[0].set_linestyle('--')
+        self.canvas.draw()
+
 
     def update_request(self, message):
 
